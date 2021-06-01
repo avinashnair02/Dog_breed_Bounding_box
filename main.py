94ecdc71c27c26f4b07ba98df3dab98fe5ca9579
Avinash Nair <avinashnair02@gmail.com>
2021-06-01 18:36:43 +0530

    initial command

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import torch
 from IPython.display import Image, clear_output  # to display images
 import os
+import cv2
 from flask import Flask, redirect, url_for, request, render_template
 
 
 app = Flask(__name__)
+
 # routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
@@ -18,7 +20,7 @@ def get_output():
         img_path = "static/" + img.filename
         img.save("static/im2.jpg")
 
-      
+        #cv2.imwrite('img1.jpg', image)
 
 
         command = "python yolov5/detect.py --weights yolov5/best.pt --img 416 --conf 0.4 --source static/im2.jpg"
@@ -34,8 +36,6 @@ def get_output():
 #@app.route('/predict', methods=['POST'])
 #def home():
 #return render_template('video.html')
-
-
-
 if __name__ == "__main__":
     app.run(debug=True)
+
